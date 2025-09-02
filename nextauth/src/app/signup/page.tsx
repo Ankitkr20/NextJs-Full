de99d21630c193244c4d634b181cf2881045ad25
Ankitkr20 <18krankit@gmail.com>
2025-09-03 02:29:13 +0530

    signup frontend

--- nextauth/src/app/signup/page.tsx
+++ nextauth/src/app/signup/page.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+'use client'
 
-function page() {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+export default function SignupPage() {
+  const router = useRouter();
+
+  const [user, setUser] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+
+      console.log("Signup Success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup Failed");
+      toast.error(error.message || "Signup Failed");
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading? "Processing":"Signup"}</h1>
+    </div>
   )
 }
-
-export default page
