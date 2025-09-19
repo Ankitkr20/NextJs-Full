bd54e6eac1ef3064d18da9bf7126368d7e5ffabd
Ankitkr20 <18krankit@gmail.com>
2025-09-19 12:10:21 +0530

    new

--- nextjsmega/src/app/(auth)/sign-in/page.tsx
+++ nextjsmega/src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client"
+import {useSession, signIn, signOut} from "next-auth/react"
 
-function page() {
-  return (
-    <div>page</div>
-  )
+export default function Page(){
+    const { data: session, status} = useSession()
+    if(status === "loading"){
+        return <p>Loading...</p>
+    }
+    if(session) {
+        return(
+            <>
+            Signed in as {session.user?.email ?? "Unknown User"}<br/>
+            <button className="bg-green-300 px-3 py-1 m-4 rounded" onClick={()=> signOut()}>Sign Out</button>
+            </>
+        )
+    }
+    return(
+        <>
+        Not Signed in <br />
+        <button className="bg-blue-100 text-black px-3 py-1 m-4 rounded"onClick={()=>signIn()}>Sign in</button>
+        </>
+    )
 }
-
-export default page
